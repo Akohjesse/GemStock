afbf58a26ea5d78a4a6c0c0c8ec1e78add146e78
akohjesse <akohgodwinjesse@gmail.com>
2021-09-28 22:10:19 +0100

    fixed

--- routes/user.js
+++ routes/user.js
@@ -24,69 +24,65 @@ router.get('/login' , (req, res)=>{
 })
 
 router.post('/signup', [ check("email", "Please enter a valid username").isEmail()],  (req, res)=>{
-     const {username,email, password} = req.body;
+    const {username,email, password} = req.body;
     let errors = []  
   
     const result = validationResult(req);
-    if(!result.isEmpty()){
-        let error = result.array();
-        errors.push({msg: error[0].msg})
-    }
-   if(password.length < 6){
-     errors.push({msg: 'Password must be atleast 6 characters'})
+       if(!result.isEmpty()){
+         let error = result.array();
+         errors.push({msg: error[0].msg})
    }
-   if(password.length > 23){
-    errors.push({msg: 'Password must not be more than 30 characters'})
-  }  
-  if(errors.length > 0 ) {
-
-        res.render('signup', {
-                errors : errors,
-                name : username,
-                email : email,
-                password : password,
-       
-         })
+    if(password.length < 6){
+         errors.push({msg: 'Password must be atleast 6 characters'})
+    }
+    if(password.length > 23){
+          errors.push({msg: 'Password must not be more than 30 characters'})
+   }  
+   if(errors.length > 0 ) {
+     res.render('signup', {
+     errors : errors,
+     name : username,
+     email : email,
+     password : password,
+     })
     }
     else{
-       
-    User.findOne({email : email}).exec((err,user)=>{ 
-        if(user) {
-           res.render('signup', {
-               errors:{
-                   msg: 'User already exists, Login instead'
-               }    
-           })
-           
-        } 
-        else {
-            const user = new User({
-                username : username,
-                email : email,
-                password : password
-            });
-            
-            bcrypt.genSalt(10, (err, salt)=>{
-                bcrypt.hash(user.password, salt, (err,hash)=>{
-                    if(err) throw err;
-                    user.password = hash;
-                  user.save(async (err)=>{
+        User.findOne({email : email}).exec((err,user)=>{ 
+            if(user) {
+               res.render('signup', {
+                   errors:[{
+                       msg: 'User already exists, Login instead'
+                   } ] 
+               })
+               
+            } 
+            else {
+                const user = new User({
+                    username : username,
+                    email : email,
+                    password : password
+                });
+                
+                bcrypt.genSalt(10, (err, salt)=>{
+                    bcrypt.hash(user.password, salt, (err,hash)=>{
                         if(err) throw err;
-                        
-                        else  var posts = await Post.find({}); posts = posts.reverse(); res.render('dashboard', {
-                            name: user.username,
-                            decrypt,
-                            from: 'Welcome To Expert Signals',
-                            posts
+                        user.password = hash;
+                      user.save(async (err)=>{
+                            if(err) throw err;
+                            
+                            else  var posts = await Post.find({}); posts = posts.reverse(); res.render('dashboard', {
+                                name: user.username,
+                                decrypt,
+                                from: 'Welcome To Expert Signals',
+                                posts
+                            })
                         })
+                        
                     })
-                    
                 })
-            })
-        }
-    })
+            }
+        })
     }
-
 });
 
 router.post('/login',  (req,res,next)=> { passport.authenticate('local',
